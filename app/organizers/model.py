396508f8731633c989f60f46f267527b9e6db752
Lucas <lveron@prismamp.com>
2024-06-14 20:20:43 -0300

    impl organizer status impl

--- app/organizers/model.py
+++ app/organizers/model.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, String, ForeignKey
+from enum import Enum
+from sqlalchemy import Column, String, ForeignKey, DateTime
 from app.database.database import Base
 from sqlalchemy.orm import relationship
 from app.utils.models_utils import DateTemplate
+from datetime import datetime, timedelta
+
+
+class InvitationStatus(str, Enum):
+    INVITED = "INVITED"
+    ACCEPTED = "ACCEPTED"
+    REJECTED = "REJECTED"
 
 
 class OrganizerModel(DateTemplate, Base):
@@ -9,6 +17,11 @@ class OrganizerModel(DateTemplate, Base):
 
     id_organizer = Column(String, ForeignKey("users.id"), primary_key=True)
     id_event = Column(String, ForeignKey("events.id"), primary_key=True)
+    invitation_expiration_date = Column(DateTime,
+                                        default=datetime
+                                        .now()+timedelta(days=365),
+                                        nullable=False)
+    invitation_status = Column(String, default=InvitationStatus.INVITED.value)
 
     organizer = relationship("UserModel", back_populates="organizers")
     event = relationship("EventModel", back_populates="organizers")
