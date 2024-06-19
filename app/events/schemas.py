e1df546e6b014a63dadbbb2d01b9afef24142cf5
mateocapon <mcapon@fi.uba.ar>
2024-06-19 16:12:26 -0300

    add default None for some fields

--- app/events/schemas.py
+++ app/events/schemas.py
@@ -7,18 +7,28 @@ from typing_extensions import Self
 class EventSchema(BaseModel):
     title: str = Field(min_length=2, max_length=100,
                        examples=["CONGRESO DE QUIMICA"])
-    start_date: datetime = Field(examples=[datetime(2024, 8, 1)])
-    end_date: datetime = Field(examples=[datetime(2024, 8, 2)])
     description: str = Field(max_length=1000, examples=["Evento en FIUBA"])
     event_type: EventType = Field(examples=[EventType.CONFERENCE])
-    location: str = Field(max_length=200,
-                          examples=["FIUBA, Av. Paseo Colon 850"])
-    tracks: str = Field(max_length=1000, examples=["track1, track2, track3"])
+    start_date: datetime | None = Field(examples=[datetime(2024, 8, 1)],
+                                        default=None)
+    end_date: datetime | None = Field(examples=[datetime(2024, 8, 2)],
+                                      default=None)
+    location: str | None = Field(max_length=200,
+                                 examples=["FIUBA, Av. Paseo Colon 850"],
+                                 default=None)
+    tracks: str | None = Field(max_length=1000,
+                               examples=["track1, track2, track3"],
+                               default=None)
 
     @model_validator(mode='after')
     def check_dates(self) -> Self:
         start_date = self.start_date
         end_date = self.end_date
+        if start_date is None and end_date is None:
+            return self
+        if start_date is None or end_date is None:
+            raise ValueError('Both start_date and"\
+                             " end_date must be specified.')
         if start_date < datetime.now() or end_date < start_date:
             raise ValueError('Invalid Dates.')
         return self
