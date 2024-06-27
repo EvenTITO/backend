d3ae3c9d46d26bbe48978c8697d2f2af39b88619
Lucas <lveron@prismamp.com>
2024-06-27 17:50:40 -0300

    add test

--- app/events/schemas.py
+++ app/events/schemas.py
@@ -84,6 +84,12 @@ class ReviewerSchema(BaseModel):
                                examples=["track1, track2, track3"],
                                default=None)
 
+    @model_validator(mode='after')
+    def check_dates(self) -> Self:
+        if self.invitation_expiration_date <= datetime.now():
+            raise ValueError('Invalid invitation expiration date.')
+        return self
+
 
 class ReviewerSchemaComplete(ReviewerSchema):
     id_user: str = Field(examples=["..."])
