09e9f592617dc81ad02ee89eb6f3aa862ae5d162
mateocapon <mcapon@fi.uba.ar>
2024-07-27 14:46:05 -0300

    configuration get has pricing and dates

--- tests/events/test_get_general_configuration.py
+++ tests/events/test_get_general_configuration.py
@@ -1,7 +1,7 @@
 from ..common import create_headers
 
 
-async def test_get_event_configuration_organizer(
+async def test_get_event_configuration_organizer_gets_dates_pricing_review_skeleton_and_emails(
     client,
     user_data,
     event_creator_data,
@@ -11,5 +11,11 @@ async def test_get_event_configuration_organizer(
         f"/events/{event_from_event_creator}/configuration",
         headers=create_headers(event_creator_data["id"])
     )
+    event_config = response.json()
 
     assert response.status_code == 200
+    print(response.json())
+    assert event_config['dates'] is None
+    assert event_config['pricing'] is None
+    assert len(event_config['notification_mails']) == 0
+    assert event_config['review_skeleton'] is None
