9f5dc9726f0e94ac18f45520820ef957888b830f
mateocapon <mcapon@fi.uba.ar>
2024-07-30 16:03:28 -0300

    all tests passing ok

--- app/repository/users_crud.py
+++ app/repository/users_crud.py
@@ -7,11 +7,6 @@ async def get_user_by_id(db: AsyncSession, user_id: str):
     return await db.get(UserModel, user_id)
 
 
-async def get_admin_emails(db: AsyncSession):
-    query = select(UserModel.email).where(UserModel.role == UserRole.ADMIN)
-    result = await db.execute(query)
-    return result.scalars().all()
-
 
 async def get_user_by_email(db: AsyncSession, email: str):
     query = select(UserModel).where(UserModel.email == email)
