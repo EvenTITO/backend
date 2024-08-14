c2d8276469c4c8c8c440901a2ed79f5522381e60
mateocapon <mcapon@fi.uba.ar>
2024-08-13 21:04:45 -0300

    fix submissions service dep

--- app/services/submissions/author_submissions_service_dep.py
+++ app/services/submissions/author_submissions_service_dep.py
@@ -6,7 +6,6 @@ from app.authorization.caller_id_dep import CallerIdDep
 from app.repository.repository import get_repository
 from app.repository.submissions_repository import SubmissionsRepository
 from app.repository.works_repository import WorksRepository
-from app.services.storage.work_storage_service import WorkStorageService
 from app.services.storage.work_storage_service_dep import WorkStorageServiceDep
 from app.services.submissions.author_submissions_service import AuthorSubmissionsService
 from app.services.works.author_works_service import AuthorWorksService
@@ -18,7 +17,7 @@ class AuthorSubmissions:
             user_id: CallerIdDep,
             event_id: str,
             work_id: int,
-            storage_service: WorkStorageService = WorkStorageServiceDep,
+            storage_service: WorkStorageServiceDep,
             submission_repository: SubmissionsRepository = Depends(get_repository(SubmissionsRepository)),
             work_repository: WorksRepository = Depends(get_repository(WorksRepository))
     ) -> AuthorSubmissionsService:
