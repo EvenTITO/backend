bf7d1791f2e1f407c9b5379252cbc42263f34007
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-08-13 22:36:02 -0300

    STORAGE fix test
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- tests/storage/test_events_storage.py
+++ tests/storage/test_events_storage.py
@@ -22,7 +22,7 @@ async def test_get_event_upload_url_must_be_event_organizer(
     event_from_event_creator,
 ):
     response = await client.get(
-        f"events/{event_from_event_creator}/upload_url/main_image",
+        f"/events/{event_from_event_creator}/upload_url/main_image",
         headers=create_headers(organizer_id_from_event)
     )
 
