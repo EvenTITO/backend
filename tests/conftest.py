5fca7dd2f9a801b378ca11209292a31e66e278ed
mateocapon <mcapon@fi.uba.ar>
2024-08-15 19:34:23 -0300

    Change mock for storage tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -72,22 +72,30 @@ async def client(session_override):
 
 @pytest.fixture(scope="function")
 async def mock_storage(mocker):
-    generate_upload_url_mock = mocker.patch(
-        'app.services.storage.storage_clients.gcp_storage_client.GCPStorageClient.generate_signed_upload_url'
-    )
-    generate_upload_url_mock.return_value = UploadURLSchema(
-        upload_url='mocked-url-upload',
-        expiration_time_seconds=3600,
-        max_upload_size_mb=5
-    )
+    base_path = 'app.services.storage.storage_clients'
+    gcp_path = base_path + '.gcp_storage_client.GCPStorageClient'
+    no_client_path = base_path + '.no_storage_provided_client.NoStorageProvidedClient'
 
-    generate_download_url_mock = mocker.patch(
-        'app.services.storage.storage_clients.gcp_storage_client.GCPStorageClient.generate_signed_read_url'
-    )
-    generate_download_url_mock.return_value = DownloadURLSchema(
-        download_url='mocked-url-download',
-        expiration_time_seconds=3600,
-    )
+    def mock_generate_upload_url(method_to_mock):
+        mock_value = mocker.patch(method_to_mock)
+        mock_value.return_value = UploadURLSchema(
+            upload_url='mocked-url-upload',
+            expiration_time_seconds=3600,
+            max_upload_size_mb=5
+        )
+
+    def mock_generate_download_url_mock(method_to_mock):
+        mock_value = mocker.patch(method_to_mock)
+        mock_value.return_value = DownloadURLSchema(
+            download_url='mocked-url-download',
+            expiration_time_seconds=3600,
+        )
+
+    mock_generate_upload_url(gcp_path+'.generate_signed_upload_url')
+    mock_generate_download_url_mock(gcp_path+'.generate_signed_read_url')
+
+    mock_generate_upload_url(no_client_path+'.generate_signed_upload_url')
+    mock_generate_download_url_mock(no_client_path+'.generate_signed_read_url')
 
     yield
 
