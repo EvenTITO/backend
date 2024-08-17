e7a36116100fa109cdd561685223edd946f7d6c8
mateocapon <mcapon@fi.uba.ar>
2024-08-17 16:27:11 -0300

    events storage - test all media kinds

--- tests/storage/test_events_storage.py
+++ tests/storage/test_events_storage.py
@@ -1,3 +1,4 @@
+from app.services.storage.event_storage_service import EventsStaticFiles
 from ..commontest import create_headers
 
 
@@ -11,7 +12,9 @@ async def test_get_event_contains_event_public_url(
 
     assert response.status_code == 200
     assert response.json()["title"] == create_event["title"]
-    assert response.json()["media"][0]["name"] == 'main_image'
+    assert response.json()["media"][0]["name"] == EventsStaticFiles.MAIN_IMAGE
+    assert response.json()["media"][0]["name"] == EventsStaticFiles.BROCHURE
+    assert response.json()["media"][0]["name"] == EventsStaticFiles.BANNER_IMAGE
     assert len(response.json()["roles"]) == 0
 
 
@@ -21,13 +24,14 @@ async def test_get_event_upload_url_must_be_event_organizer(
     create_organizer,
     create_event_from_event_creator,
 ):
-    response = await client.get(
-        f"/events/{create_event_from_event_creator}/upload_url/main_image",
-        headers=create_headers(create_organizer)
-    )
+    for event_type in EventsStaticFiles:
+        response = await client.get(
+            f"/events/{create_event_from_event_creator}/upload_url/{event_type.value}",
+            headers=create_headers(create_organizer)
+        )
 
-    assert response.status_code == 200
-    assert response.json()["upload_url"] is not None
+        assert response.status_code == 200
+        assert response.json()["upload_url"] is not None
 
 
 async def test_get_event_upload_url_not_organizer_fails(
@@ -42,3 +46,17 @@ async def test_get_event_upload_url_not_organizer_fails(
     )
 
     assert response.status_code == 403
+
+
+async def test_get_event_upload_url_nonexistent_file_fails(
+    mock_storage,
+    client,
+    create_organizer,
+    create_event_from_event_creator,
+):
+    response = await client.get(
+        f"/events/{create_event_from_event_creator}/upload_url/nonexistent_image",
+        headers=create_headers(create_organizer)
+    )
+
+    assert response.status_code == 422
