3fa4552272801888f7431c86f73fa13444ed3113
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-08-21 00:23:06 -0300

    fix alembic
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- app/database/models/member.py
+++ app/database/models/member.py
@@ -1,8 +1,16 @@
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import declarative_mixin, declared_attr
 
 from app.database.models.utils import DateTemplate
 
 
+@declarative_mixin
 class MemberModel(DateTemplate):
-    user_id = Column(String, ForeignKey("users.id"), primary_key=True)
-    event_id = Column(String, ForeignKey("events.id"), primary_key=True)
+
+    @declared_attr
+    def user_id(self):
+        return Column(String, ForeignKey("users.id"), primary_key=True)
+
+    @declared_attr
+    def event_id(self):
+        return Column(String, ForeignKey("events.id"), primary_key=True)
