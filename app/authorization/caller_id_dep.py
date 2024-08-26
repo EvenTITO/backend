1d83de9723d90aa56469ed460910c85d3d3f4fc3
mateocapon <mcapon@fi.uba.ar>
2024-08-25 21:42:09 -0300

    remove warnings

--- app/authorization/caller_id_dep.py
+++ app/authorization/caller_id_dep.py
@@ -1,20 +1,11 @@
 from typing import Annotated
-from fastapi import HTTPException, Header, Depends
-from pydantic import BaseModel, ValidationError
+from fastapi import Header, Depends
 
 from app.schemas.users.utils import UID
 
 
-class UIDModel(BaseModel):
-    uid: UID
-
-
-async def verify_user_id(X_User_Id: Annotated[UID, Header()]) -> UID:
+async def verify_user_id(X_User_Id: Annotated[UID, Header()]) -> str:
     return X_User_Id
-    try:
-        return UIDModel(uid=X_User_Id).uid  # force validation
-    except ValidationError:
-        raise HTTPException(status_code=400, detail="X-User-Id missing.")
 
 
-CallerIdDep = Annotated[UID, Depends(verify_user_id)]
+CallerIdDep = Annotated[str, Depends(verify_user_id)]
