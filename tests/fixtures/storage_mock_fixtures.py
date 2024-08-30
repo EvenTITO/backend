4c4dc118dbedb8d255971f720839d91505bc9e10
mateocapon <mcapon@fi.uba.ar>
2024-08-30 00:22:15 -0300

    remove mock from all tests. Add session fixture

--- tests/fixtures/storage_mock_fixtures.py
+++ tests/fixtures/storage_mock_fixtures.py
@@ -1,32 +1,46 @@
-import pytest
 from app.schemas.storage.schemas import DownloadURLSchema, UploadURLSchema
+import pytest
+from unittest.mock import AsyncMock, patch
 
 
-@pytest.fixture(scope="function")
-async def mock_storage(mocker):
+@pytest.fixture(scope="session", autouse=True)
+def mock_storage():
     base_path = 'app.services.storage.storage_clients'
-    gcp_path = base_path + '.gcp_storage_client.GCPStorageClient'
-    no_client_path = base_path + '.no_storage_provided_client.NoStorageProvidedClient'
+    gcp_path = f'{base_path}.gcp_storage_client.GCPStorageClient'
+    no_client_path = f'{base_path}.no_storage_provided_client.NoStorageProvidedClient'
 
-    def mock_generate_upload_url(method_to_mock):
-        mock_value = mocker.patch(method_to_mock)
-        mock_value.return_value = UploadURLSchema(
+    async def mock_generate_upload_url(*args, **kwargs):
+        return UploadURLSchema(
             upload_url='mocked-url-upload',
             expiration_time_seconds=3600,
             max_upload_size_mb=5
         )
 
-    def mock_generate_download_url_mock(method_to_mock):
-        mock_value = mocker.patch(method_to_mock)
-        mock_value.return_value = DownloadURLSchema(
+    async def mock_generate_download_url(*args, **kwargs):
+        return DownloadURLSchema(
             download_url='mocked-url-download',
-            expiration_time_seconds=3600,
+            expiration_time_seconds=3600
         )
 
-    mock_generate_upload_url(gcp_path+'.generate_signed_upload_url')
-    mock_generate_download_url_mock(gcp_path+'.generate_signed_read_url')
+    patches = [
+        patch(
+            f'{gcp_path}.generate_signed_upload_url', new_callable=AsyncMock, side_effect=mock_generate_upload_url
+        ),
+        patch(
+            f'{gcp_path}.generate_signed_read_url', new_callable=AsyncMock, side_effect=mock_generate_download_url
+        ),
+        patch(
+            f'{no_client_path}.generate_signed_upload_url', new_callable=AsyncMock, side_effect=mock_generate_upload_url
+        ),
+        patch(
+            f'{no_client_path}.generate_signed_read_url', new_callable=AsyncMock, side_effect=mock_generate_download_url
+        )
+    ]
 
-    mock_generate_upload_url(no_client_path+'.generate_signed_upload_url')
-    mock_generate_download_url_mock(no_client_path+'.generate_signed_read_url')
+    for p in patches:
+        p.start()
 
     yield
+
+    for p in patches:
+        p.stop()
