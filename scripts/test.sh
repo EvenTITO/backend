c120e77a32925fbf9130bbb9341b618c699dafd7
mateocapon <mcapon@fi.uba.ar>
2024-09-02 20:42:53 -0300

    fix dependencies issues

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,1 @@
-docker compose -f docker-compose-dev.yaml exec backend bash -c "pip install httpx pytest pytest-mock; exec bash"
+docker compose -f docker-compose-dev.yaml exec backend bash -c "pip install -r requirements-dev.txt; exec bash"
--- scripts/up.sh
+++ scripts/up.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 docker build -f ./Dockerfile -t backend:latest .
+NETWORK_NAME="eventito-dev-network"
+
+if ! docker network ls --format '{{.Name}}' | grep -wq "$NETWORK_NAME"; then
+    echo "Creating network '$NETWORK_NAME'..."
+    docker network create "$NETWORK_NAME"
+fi
+
 docker compose -f docker-compose-dev.yaml up --build -d
