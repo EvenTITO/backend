27599fb80886eed794478060e3c3be62e4a27a75
mateocapon <mcapon@fi.uba.ar>
2024-09-25 22:02:43 -0300

    change value to answer

--- app/schemas/events/review_skeleton/multiples_choice_question.py
+++ app/schemas/events/review_skeleton/multiples_choice_question.py
@@ -14,12 +14,12 @@ class MultipleChoiceQuestion(BaseModel):
 
 
 class MultipleChoiceAnswer(MultipleChoiceQuestion):
-    answers: list[str] = Field(examples=[['first answer', 'second answer']])
+    answer: list[str] = Field(examples=[['first answer', 'second answer']])
 
     @model_validator(mode='after')
-    def check_answers(self) -> Self:
-        if not self.more_than_one_answer_allowed and len(self.answers) > 1:
+    def check_answer(self) -> Self:
+        if not self.more_than_one_answer_allowed and len(self.answer) > 1:
             raise ValueError("Only one answer allowed")
-        if not all(answer in self.options for answer in self.answers):
-            raise ValueError("All answers should be contained in options")
+        if not all(answer in self.options for answer in self.answer):
+            raise ValueError("All answer should be contained in options")
         return self
