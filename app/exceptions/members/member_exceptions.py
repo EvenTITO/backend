3fcc3e164b45d9831b503b9dc1a97a299f1af455
mateocapon <mcapon@fi.uba.ar>
2024-09-28 18:21:04 -0300

    change status code for member already exist

--- app/exceptions/members/member_exceptions.py
+++ app/exceptions/members/member_exceptions.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 
 class AlreadyMemberExist(HTTPException):
     def __init__(self, event_id, user_id, role):
-        self.status_code = 403
+        self.status_code = 409
         self.detail = f"Already member for user_id: {user_id} in event: {event_id} with role: {role}"
         super().__init__(status_code=self.status_code, detail=self.detail)
 
