eec6c074a8de07fbe466078d5b8a00ec0a9a4377
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-09-30 01:06:06 -0300

    event_creator - added creator in public events in admin request
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- app/schemas/events/public_event.py
+++ app/schemas/events/public_event.py
@@ -1,11 +1,12 @@
 from uuid import UUID
 
-from pydantic import computed_field
+from pydantic import computed_field, BaseModel, Field, ConfigDict, PrivateAttr
 
 from app.schemas.events.event_status import EventStatusSchema
 from app.schemas.events.review_skeleton.review_skeleton import ReviewSkeletonResponseSchema
 from app.schemas.events.schemas import StaticEventSchema, DynamicEventSchema
 from app.schemas.media.image import ImgSchema
+from app.schemas.users.utils import UID
 from app.services.storage.event_storage_service import EventsStorageService
 
 
@@ -15,3 +16,20 @@ class PublicEventSchema(StaticEventSchema, DynamicEventSchema, ReviewSkeletonRes
     @computed_field
     def media(self) -> list[ImgSchema]:
         return EventsStorageService.get_media(self.id)
+
+
+class EventCreatorSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: UID
+    name: str = Field(exclude=True)
+    lastname: str = Field(exclude=True)
+    email: str = Field(examples=["jose.perez@email.com"])
+
+    @computed_field
+    def fullname(self) -> str:
+        return self.name + " " + self.lastname
+
+
+class PublicEventWithCreatorSchema(PublicEventSchema):
+    model_config = ConfigDict(from_attributes=True)
+    creator: EventCreatorSchema | None = Field(default=None)
